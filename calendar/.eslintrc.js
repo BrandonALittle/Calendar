a52899d2a63797f8b97d491bbb5caaf5c42ecd77
Brandon Little <brandon.little@geniussportsmedia.com>
2019-11-03 10:41:16 -0500

    :wrench: Initial lint and format setup

---
+++ calendar/.eslintrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+    plugins: ['react'],
+    extends: [
+        'eslint:recommended',
+        'plugin:react/recommended',
+    ],
+    parserOptions: {
+        ecmaVersion: 6,
+        sourceType: 'module',
+        ecmaFeatures: {
+            jsx: true,
+        },
+    },
+    settings: {
+        react: {
+            version: 'detect'
+        }
+    },
+    env: {
+        node: true,
+    },
+    rules: {
+        quotes: ['error', 'single', { avoidEscape: true }],
+        'comma-dangle': ['error', 'always-multiline']
+    },
+}
---
+++ calendar/.prettierrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+    useTabs: false,
+    printWidth: 80,
+    tabWidth: 4,
+    singleQuote: true,
+    trailingComma: "all",
+    semi: false
+}
