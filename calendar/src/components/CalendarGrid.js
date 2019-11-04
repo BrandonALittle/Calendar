f29de4bf0b8b03905c0952ebc703c2f4db56326b
Brandon Little <brandon.little@geniussportsmedia.com>
2019-11-04 18:36:28 -0500

    Add weekday names to calendar

--- calendar/src/components/CalendarGrid.js
+++ calendar/src/components/CalendarGrid.js
@@ -12,11 +12,29 @@ import styled from 'styled-components'
 import CalendarDay from './CalendarDay'
 
 const StyledCalendarGrid = styled.div`
+    grid-area: "calendar-grid";
     display: grid;
     grid-template-columns: repeat(7, 1fr);
-    grid-template-rows: repeat(auto-fit, minmax(150px, 1fr));
-    height: 100vh;
+    grid-template-rows: 30px repeat(5, 1fr);
+    @media (max-width: 540px) {
+        display: flex;
+        flex-direction: column;
+        > p {
+            display: none;
+        }
+    }
 `
+
+const StyledWeekday = styled.p`
+    margin: 5px 0;
+    text-align: center;
+    font-family: "Trebuchet MS", Helvetica, sans-serif;
+`
+
+const WeeksBar = () => {
+    const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+    return days.map(day => <StyledWeekday>{day}</StyledWeekday>)
+}
 
 const CalendarGrid = props => {
     const getDaysOfMonth = date => {
@@ -33,8 +51,8 @@ const CalendarGrid = props => {
     }
 
     return (
-        // <WeeksBar />
         <StyledCalendarGrid>
+            <WeeksBar className="weekday-names" />
             {getDaysOfMonth(props.currentDate).map(day => {
                 return (
                     <CalendarDay
