d5f9cab2ff398063d93f56859b33e4ec70af8c58
Brandon Little <brandon.little@geniussportsmedia.com>
2019-11-04 14:27:30 -0500

    Add schedule context to CalendarDay

--- calendar/src/components/CalendarDay.js
+++ calendar/src/components/CalendarDay.js
@@ -1,27 +1,40 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { isToday, getDate } from 'date-fns'
 import styled from 'styled-components'
+import Reminder from './Reminder'
+import Schedule from './ScheduleContext'
 
 const Day = styled.div`
     padding: 10px;
     border: solid 1px black;
-    background-color: ${props => props.isThisMonth ? "white" : "gray"};
+    background-color: ${props => (props.isThisMonth ? 'white' : 'gray')};
     opacity: 0.5;
-    border: ${props => props.isToday ? "solid green 2px" : "none"};
-    box-shadow: ${props => props.isSelected? '2px 2px 3px black inset, -2px -2px 3px black inset' : 'none'};
+    border: ${props => (props.isToday ? 'solid green 2px' : 'none')};
+    box-shadow: ${props =>
+        props.isSelected
+            ? '2px 2px 3px black inset, -2px -2px 3px black inset'
+            : 'none'};
+    overflow: scroll;
 `
 
 const CalendarDay = props => {
-    const today = isToday(props.date)
+    const schedule = useContext(Schedule)
+    const { date, isThisMonth, isSelected, handleSelectDate } = props
+    const today = isToday(date)
+    const dayOfMonth = getDate(date)
+    const reminders = schedule.getRemindersForDate(date)
 
     return (
-        <Day 
-            isThisMonth={props.isThisMonth} 
-            isToday={today} 
-            onClick={() => props.handleSelectDate(props.date)}
-            isSelected={props.isSelected}
+        <Day
+            isThisMonth={isThisMonth}
+            isToday={today}
+            onClick={() => handleSelectDate(date)}
+            isSelected={isSelected}
         >
-            {getDate(props.date)}
+            {dayOfMonth}
+            {reminders.map(reminder => (
+                <Reminder reminder={reminder} />
+            ))}
         </Day>
     )
 }
