8a504b0ce75f2aabcce62174967a6245171c2d23
Brandon Little <brandon.little@geniussportsmedia.com>
2019-11-04 14:25:38 -0500

    Add ScheduleProvider to Calendar

--- calendar/src/components/Calendar.js
+++ calendar/src/components/Calendar.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import { format, addMonths, subMonths } from 'date-fns'
 import styled from 'styled-components'
+import ScheduleProvider from './ScheduleProvider'
 import CalendarHeader from './CalendarHeader'
 import CalendarGrid from './CalendarGrid'
 
@@ -31,25 +32,30 @@ export default class Calendar extends Component {
         })
     }
 
-    handleSelectDate = (date) => {
+    handleSelectDate = date => {
         this.setState({
-            selectedDate: date
+            selectedDate: date,
         })
     }
 
     render() {
         return (
-            
-            <div>
-                <HeaderDiv>
-                    <CalendarHeader 
-                        currentDate={this.state.currentDate} 
-                        incrementMonth={this.incrementMonth}
-                        decrementMonth={this.decrementMonth}
+            <ScheduleProvider>
+                <div style={{width: '100vw'}}>
+                    <HeaderDiv>
+                        <CalendarHeader
+                            currentDate={this.state.currentDate}
+                            incrementMonth={this.incrementMonth}
+                            decrementMonth={this.decrementMonth}
+                        />
+                    </HeaderDiv>
+                    <CalendarGrid
+                        currentDate={this.state.currentDate}
+                        selectedDate={this.state.selectedDate}
+                        handleSelectDate={this.handleSelectDate}
                     />
-                </HeaderDiv>
-                <CalendarGrid currentDate={this.state.currentDate} selectedDate={this.state.selectedDate} handleSelectDate={this.handleSelectDate}></CalendarGrid>
-            </div>
+                </div>
+            </ScheduleProvider>
         )
     }
 }
