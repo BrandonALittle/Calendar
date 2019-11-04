c78c337246726546b3e5154c72fbbdd022fa47c6
Brandon Little <brandon.little@geniussportsmedia.com>
2019-11-04 18:36:10 -0500

    Modify styles of reminder

--- calendar/src/components/Reminder.js
+++ calendar/src/components/Reminder.js
@@ -1,16 +1,50 @@
 import React from 'react'
 import { format } from 'date-fns'
+import styled from 'styled-components'
+
+const StyledReminder = styled.div`
+    margin-bottom: 5px;
+    background-color: ${props => props.color || 'light-gray'};
+    opacity: 0.8;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    font-family: Roboto;
+    position: relative;
+    border-radius: 2px;
+    box-shadow: 1px 1px 2px gray, -1px -1px 2px gray;
+`
+
+const StyledTime = styled.div`
+    font-weight: bold;
+    writing-mode: vertical-lr;
+    padding: 2px;
+    background-color: white;
+`
+
+const StyledText = styled.p`
+    padding-left: 5px;
+    margin: 0px;
+`
+
+const StyledCity = styled.div`
+    margin: 0;
+    padding: 5px;
+    text-decoration: underline;
+`
 
 const Reminder = props => {
     const { reminder } = props
     const time = format(reminder.timeStamp, "h':'mm")
 
     return (
-        <div>
-            <h4>{time}</h4>
-            <p>{reminder.text}</p>
-            <p>{reminder.city}</p>
-        </div>
+        <StyledReminder color={reminder.color}>
+            <div>
+                <StyledText>{reminder.text}</StyledText>
+                <StyledCity>{reminder.city}</StyledCity>
+            </div>
+            <StyledTime>{time}</StyledTime>
+        </StyledReminder>
     )
 }
 
